d428dab1df2e829ae6c9569295848c20007210bd
rukaury <aruka102@uottawa.ca>
2019-05-15 15:16:35 -0400

    Restructured repo, added api authentication (register, login and logout)

---
+++ app/config.py
@@ -0,0 +1,30 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+postgres_local_base = 'postgresql://postgres:password@localhost:5432/'
+database_name = 'api'
+
+
+class BaseConfig:
+    """
+    Base application configuration
+    """
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_strong_key')
+    BCRYPT_HASH_PREFIX = 14
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AUTH_TOKEN_EXPIRY_DAYS = 30
+    AUTH_TOKEN_EXPIRY_SECONDS = 3600
+    EVENTS_PER_PAGE = 25
+
+
+class DevelopmentConfig(BaseConfig):
+    """
+    Development application configuration
+    """
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', postgres_local_base + database_name)
+    BCRYPT_HASH_PREFIX = 4
+    AUTH_TOKEN_EXPIRY_DAYS = 1
+    AUTH_TOKEN_EXPIRY_SECONDS = 20
+    EVENTS_PER_PAGE = 4
